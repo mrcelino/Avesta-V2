77600a94e4bb6e43ca0d2578cee953e31a18fbbc
Marcelino <marcelinokenny12@gmail.com>
2025-03-06 00:59:57 +0700

    Feat:  Add API routes for authentication

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [LoginController::class, 'apiLogin']);
+});
