8539b792e9fe5a2e751643de3d401de1d7e1c8ac
Marcelino <marcelinokenny12@gmail.com>
2025-03-07 00:22:15 +0700

    Configure font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.tsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.tsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans], // Set Poppins sebagai default font-sans
+            },
+            backgroundImage: {
+                "hero-bg": "url('/image/bghero.png')",
+            },
+            colors: {
+                pink: "#FB657A",
+                heading: "#F14C4C",
+                cInput: "#F4F4F4",
             },
         },
     },
