8fc9b57408d4465ba97a5ff7580919ef4037cec4
Marcelino <marcelinokenny12@gmail.com>
2025-03-07 10:53:51 +0700

    Add page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,10 +1,13 @@
 import { Link } from '@inertiajs/react';
 import Navbar from '../Components/NavbarGuest';
 import Footer from '../Components/Footer';
+import Hero from '../Components/Home/Hero';
+import WhyUs from '../Components/Home/Whyus';
 const Welcome = () => {
     return (
         <div>
             <Navbar/>
+
             <div className='py-20 min-h-screen flex items-center justify-center'>
                 <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
                 <h1>Welcome to Inertia.js</h1>
