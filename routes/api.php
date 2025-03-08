cb4c953b12434ea147564ddcaa0e89db97ad341b
Marcelino <marcelinokenny12@gmail.com>
2025-03-08 22:31:35 +0700

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
-
+use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\UserCheckController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [LoginController::class, 'apiLogin']);
     Route::post('/register', [RegisterController::class, 'apiRegister']);
 });
+
+Route::middleware('auth:sanctum')->post('/logout', [LoginController::class, 'apiLogout']);
+Route::middleware('auth:sanctum')->get('/me', [UserCheckController::class, 'me']);
+
+Route::get('/unggas', [ProductController::class, 'index']);
--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,18 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Rute untuk tamu (hanya bisa diakses jika belum login)
 Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'show'])->name('login');
     Route::get('/register/{role?}', [RegisterController::class, 'show'])->name('register');
-    Route::post('/login', [LoginController::class, 'authenticate']);
-    
 });
 
 Route::group([], function () {
     Route::get('/', fn () => Inertia::render('Welcome'));
-    Route::get('/dashboard', fn () => Inertia::render('Dashboard'));
     Route::get('/contact', fn () => Inertia::render('Guest/Contact'));
     Route::get('/about', fn () => Inertia::render('Guest/About'));
     Route::get('/product', fn () => Inertia::render('Guest/Product'));
+    Route::get('/dashboard', fn () => Inertia::render('Buyer/Dashboard'));
+    Route::get('/cariayam', fn () => Inertia::render('Buyer/CariAyam'));
+    Route::get('/tes', fn () => Inertia::render('Buyer/Tes'));
 });
