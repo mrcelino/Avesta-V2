840d9bf17b295c7f4321278daaa6737609d69c91
Marcelino <marcelinokenny12@gmail.com>
2025-03-08 19:35:07 +0700

    Page tes

---
+++ resources/js/Pages/Buyer/Tes.tsx
@@ -0,0 +1,12 @@
+import AuthLayout from "@/Layouts/AuthLayout";
+
+export default function Tes(){
+    return(
+        <AuthLayout>
+          <div className="container min-h-screen">
+            <h1>Ini adalah halaman Tes</h1>
+          </div>
+
+        </AuthLayout>
+    )
+}
